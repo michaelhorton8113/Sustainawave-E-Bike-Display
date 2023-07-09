571672ba6dc0a9f236b0ad852a9a97756a12c807
David V <Davidvans.dv@gmail.com>
2023-07-08 20:52:40 -0700

    Remove untracked debug folder

--- Debug/Drivers/BSP/Components/ili9341/ili9341_reg.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/ili9341/ili9341_reg.o: \
- ../Drivers/BSP/Components/ili9341/ili9341_reg.c \
- ../Drivers/BSP/Components/ili9341/ili9341_reg.h
-../Drivers/BSP/Components/ili9341/ili9341_reg.h:
